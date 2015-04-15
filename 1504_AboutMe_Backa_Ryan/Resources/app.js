876b08486a618922bd199b0680f8ea6afa6f5ff9
RyanBacka <RKBacka@Fullsail.edu>
2015-04-15 00:42:27 -0400

    add function to function.js and require function to app.js

--- 1504_AboutMe_Backa_Ryan/Resources/app.js
+++ 1504_AboutMe_Backa_Ryan/Resources/app.js
@@ -6,7 +6,21 @@ var mainWin = Ti.UI.createWindow({
 	backgroundColor:#fff
 });
 
+//creation of title 
+var titleView = Ti.UI.createView({
+	top: 20,
+	height: 40,
+	backgroundColor:"#d8d8d8"
+});
 
+var titleText = Ti.UI.createLabel({
+	text: "About Me",
+	font: {fontSize: 14, fontFamily: "Arial", fontWeight: "bold"},
+	color: "#fff",
+	center: titleView
+});
 
+var loadFunction = require("function");
 
-mainWin.add(qstnView,ansView);
+titleView.add(titleText);
+mainWin.add(titleView);
