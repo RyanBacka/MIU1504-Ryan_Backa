99deb6b0181c1f76bb78100a9348e9aeb0e46e6d
RyanBacka <RKBacka@Fullsail.edu>
2015-04-15 00:18:19 -0400

    First Commit create main window, create JSON data, create views

---
+++ 1504_AboutMe_Backa_Ryan/JSON.js
@@ -0,0 +1,44 @@
+//JSON data and export to app.js
+var data = {
+		"about":{
+			"headTitle":"The Questions and Answers",
+			"footTitle":"Now you know a little about me!",
+			"qstnAns":[
+				{
+					"qstn":"What is your name?",
+					"ansr":"Ryan Kane Backa"
+				},{
+					"qstn":"What is your chat user name, including the host?",
+					"ansr":"RKBacka on AIM"
+				},{
+					"qstn":"In what time zone do you reside?",
+					"ansr":"Eastern"
+				},{
+					"qstn":"Why are you in the Mobile Development program?",
+					"ansr":"I enjoy coding, and I am very interested by mobile apps, and the progression of the mobile market in its short existence."
+				},{
+					"qstn":"How comfortable are you with JavaScript?",
+					"ansr":"I am extremely comfortable with what I have learned so far."
+				},{
+					"qstn":"How comfortable are you with Titanium?",
+					"ansr":"I am comfortable with what I have learned about Titanium thus far."
+				},{
+					"qstn":"What is your favorite sport?",
+					"ansr":"Hockey"
+				},{
+					"qstn":"What kind of work do you do?",
+					"ansr":"I am self-employed as a Flowback Operator in the Pennsylvania oilfields."
+				},{
+					"qstn":"What is your favorite TV show?",
+					"ansr":"Archer"
+				},{
+					"qstn":"What is your favorite food?",
+					"ansr":"Chicken Paprikash"
+				},{
+					"qstn":"What is your favorite color?",
+					"ansr":"Yellow"
+			}]
+		}
+	};
+		
+exports.data = data;
