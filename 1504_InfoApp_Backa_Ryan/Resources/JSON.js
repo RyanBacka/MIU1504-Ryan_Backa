95671bbe23cdc97ce4343368af2c7001445f722f
RyanBacka <RKBacka@Fullsail.edu>
2015-04-16 20:01:44 -0400

    Updated JSON data. still need to add more to JSON once i build further

---
+++ 1504_InfoApp_Backa_Ryan/Resources/JSON.js
@@ -0,0 +1,52 @@
+//JSON data and export to app.js
+var data = {
+		"about":{
+			"artistInfo":[
+				{
+					"artist":"Offspring",
+					"album":"Smash",
+					"release":"April 8, 1994",
+					"genre":"Punk Rock",
+					"label":"Epitaph",
+					"billboard":"4"
+				},{
+					"artist":"Green Day,
+					"album":"Dookie",
+					"release":"February 1, 1994",
+					"genre":"Punk Rock",
+					"label":"Reprise",
+					"billboard":"2"
+				},{
+					"artist":"Sublime",
+					"album":"Sublime",
+					"release":"July 30, 1996",
+					"genre":"Ska Punk, Dub, Punk Rock, alternative Rock",
+					"label":"MCA",
+					"billboard":"13"
+				},{
+					"artist":"Deftones",
+					"album":"Around The Fur",
+					"release":"October 28, 1997",
+					"genre":"Alternative Metal, nu Metal",
+					"label":"Maverick/Warner Bros.",
+					"billboard":"29"
+				},{
+					"qstn":"Slipknot",
+					"album":"Slipknot",
+					"release":"June 29, 1999",
+					"genre":"Nu Metal",
+					"label":"Roadrunner, Attic, I AM",
+					"billboard":"51"
+				},{
+					"qstn":"Fall Out Boy",
+					"album":"From Under The Cork Tree",
+					"release":"May 3, 2005",
+					"genre":"Pop Punk, Emo Pop, Pop Rock",
+					"label":"Island",
+					"billboard":"9"
+				}
+			]
+		}
+	};
+		
+exports.data = data;
